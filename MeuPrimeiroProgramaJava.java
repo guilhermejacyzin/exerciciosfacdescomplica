c4c6a43a2addda99766cd570dd88ad9ca0c1e6a9
Guilherme Jacyzin <jacyzin@gmail.com>
2023-04-19 23:07:25 -0300

    Add files via upload

---
+++ MeuPrimeiroProgramaJava.java
@@ -0,0 +1,20 @@
+//nome da classe
+public class MeuPrimeiroProgramaJava {
+//módulo principal com a entrada pela linha de comando
+    public static void main (String entrada[])
+    {
+        int inteiro = 33;
+        char caracter = 'F';
+        double real = 1.82;
+        String frase = "Guilherme ";
+        boolean VF=true;
+
+        if (VF == true)
+        {
+            System.out.println("Eu sou o " + frase + "tenho "+ inteiro + " anos e tenho " + real + "m de altura");
+        }
+
+        System.exit(0);
+    }
+    
+}
---
+++ MinhaAtividade13.java
@@ -0,0 +1,23 @@
+public class MinhaAtividade13 {
+    
+    public static void main (String entrada[])
+    {
+        //declarando as variáveis
+        String nome = "Guilherme Jacyzin";
+        String curso = "Ciência da Computação";
+        int idade = 33;
+        char gen = 'M';
+        double real = 1.82;
+        boolean VF = true;
+
+        if (VF == true)
+        {
+            System.out.println("Eu sou o" + " " + nome + "," + 
+            " " + "curso" + " " + curso + " " + "e possuo" + " " +
+            idade + " " + "anos" + "." + " Sou do sexo" + " " + gen + " " +
+            "e tenho" + " " + real + " " + "de" + " " + "altura.");
+        }
+        System.exit(0);
+    }
+
+}
